4db6451cb6ba2d5cdd0d3244eb321d37717dc783
adalyuf <adalyuf@gmail.com>
2020-12-29 17:14:10 -0500

    Added admin functionality

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'djangogirls.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['djangogirls/templates/'],
+        'DIRS': [ BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
